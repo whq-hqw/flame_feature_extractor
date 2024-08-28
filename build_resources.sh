6ab6a82d67abe49bbd61ab551ea215cbb942a063
Nabarun Goswami <nabarungoswami@mi.t.u-tokyo.ac.jp>
2024-08-28 23:33:38 +0900

    Update build_resources.sh

--- build_resources.sh
+++ build_resources.sh
@@ -1,6 +1,6 @@
 wget https://github.com/xg-chu/lightning_track/releases/download/resources/resources.tar ./resources.tar
 tar -xvf resources.tar
-mv resources/emoca/* .flame_feature_extractor/feature_extractor/emoca/assets/
+mv resources/emoca/* ./flame_feature_extractor/feature_extractor/emoca/assets/
 mv resources/FLAME/* ./flame_feature_extractor/renderer/assets/
 #mv resources/human_matting/* ./engines/human_matting/assets/
 mv resources/mica/* ./flame_feature_extractor/feature_extractor/mica/assets/
