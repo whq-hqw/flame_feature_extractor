e9572e9d9f6f95b505db07062552a7edd734b1ba
Nabarun Goswami <nabarungoswami@mi.t.u-tokyo.ac.jp>
2024-08-28 23:20:33 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="flame_feature_extractor",
+    version="0.0.1",
+    description="Flame Feature Extraction",
+    author="Nabarun Goswami",
+    author_email="nabarungoswami@mi.t.u-tokyo.ac.jp",
+    packages=["flame_feature_extractor"],
+)
