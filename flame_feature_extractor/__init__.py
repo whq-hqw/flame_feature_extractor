766f0a3cb1ae5e16919d5ea08acc0396defe2550
HQ Wang <wang@sansan.com>
2024-08-29 13:28:14 +0900

    Update __init__.py

---
+++ flame_feature_extractor/__init__.py
@@ -0,0 +1,4 @@
+from .feature_extractor.mica.mica import MICA
+from .feature_extractor.emoca.emoca_v2 import EMOCAV2
+from .feature_extractor.flame_feature_extractor import PreProcessBatchFace
+from .feature_extractor.preprocess import PreProcessBatchFace
